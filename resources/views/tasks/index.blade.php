068d4e9e2d2a8602d248876dcfd7d8858155d2ae
shunta.nomura <ns_is_a_pen@icloud.com>
2019-12-14 09:07:03 +0000

    tasklist open

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,19 +4,20 @@
 
     <h1>タスク一覧</h1>
 
-    @if (count($tasks) > 0)
+   @if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>ステータスカラム</th>
                     <th>タスク</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id,['id' => $task->id]) !!}</td>
-                    <td>{{$task->status }}</td>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
